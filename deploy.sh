4c4666d97507868884811ef10e494fae6286f5c2
Scott Devoid <sdevoid@gmail.com>
2012-02-15 15:02:43 -0600

    Add cloudinit.sh, deploy.sh, execution perms
    - Added cloudinit.sh - pass this script as a
    cloud-init --user_data. It fetches this repo
    and installs it on the target machine.
    - Altered deploy.sh - was using dev.git when
    it needed to be dev_container.git, fixed
    directory bug.
    - Made kb_perl_runtime/build.runtime executable

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
-git clone kbase@git.kbase.us:dev.git
-cd dev/modules
-for i in `cat repos.list`; do 
+#!/bin/bash
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+git clone kbase@git.kbase.us:dev_container.git
+pushd $DIR/dev_container/modules
+for i in `cat $DIR/repos.list`; do 
   git clone kbase@git.kbase.us:${i}
 done
+popd
+
