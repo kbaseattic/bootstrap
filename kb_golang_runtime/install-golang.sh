ab52281cce5b5b686a6b453aee551c57370df18b
Jared Wilkening <jared.wilkening@gmail.com>
2012-05-15 13:58:39 -0500

    fixed bugs

--- kb_golang_runtime/install-golang.sh
+++ kb_golang_runtime/install-golang.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 pushd /usr/local
 hg clone -u release https://code.google.com/p/go
 pushd go
@@ -7,14 +8,20 @@ pushd src
 ./all.bash
 popd
 for B in `ls bin`; do
+	if [ -e /usr/local/bin/$B]; then
+		rm /usr/local/bin/$B
+	fi
 	ln -s `pwd`/bin/$B /usr/local/bin
 done
 popd
+popd
 
 export GOPATH=/usr/local/gopath
-mkdir $GOPATH
+if [ ! -e $GOPATH ]; then
+	mkdir $GOPATH
+fi
 
-for P in `cat golang-packages`; do
+for P in `cat ./golang-packages`; do
 	go get -v $P
 done
 
