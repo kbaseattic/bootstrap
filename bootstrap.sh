5a9845468f5c6bc29753be52e96a297f7af2e593
Travis Harrison <guumby@gmail.com>
2012-08-13 17:27:48 -0700

    split cloudinit.sh into 2 parts: download and set up bootstrap repo, run installs in bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# install debian dependencies
+pushd kb_bootstrap
+./install-debian-packages package-list.ubuntu
+popd
+
+# install node and npm
+pushd kb_node_runtime
+./build.node
+popd
+
+# build and install perl 
+pushd kb_perl_runtime
+./build.runtime
+popd
+
+# build and install golang
+pushd kb_golang_runtime
+./install-golang.sh
+popd
+
+# build and install python modules
+pushd kb_python_runtime
+./install-python-packages.sh
+popd
+
+# build and install qiime python modules
+pushd kb_qiime
+./install-qiime.sh
+popd
+
+# build and install oracle java
+pushd kb_java_runtime
+./java_build.sh
+popd
+
+# build and install thrift
+pushd kb_thrift_runtime
+./thrift_build.sh
+popd
+
+
