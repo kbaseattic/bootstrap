2cbfd46dfbafbce0eb6d40ee4082d89af968c4ea
Travis Harrison <guumby@gmail.com>
2012-08-14 10:24:58 -0700

    chnaged uclust path to /kb/runtime/bin

--- kb_qiime/install-qiime.sh
+++ kb_qiime/install-qiime.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 if [[ $# != 1 ]] ; then
-	echo "Usage: $0 python-qiime-list" 1>&2
-	exit 1
+    echo "Usage: $0 python-qiime-list" 1>&2
+    exit 1
 fi
 
 pkg_list=$1
 IFS=$'\t'
 
 while read PKG TGZ URL; do
+    echo "###### downloading $PKG ######"
     wget $URL
     tar zxf $TGZ
     pushd $PKG
@@ -21,6 +22,6 @@ done < $pkg_list
 
 pushd bin
 for B in `ls`; do 
-    cp $B /usr/local/bin
+    cp $B /kb/runtime/bin/.
 done
 popd
