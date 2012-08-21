4b0b21de2a0a106d4b7b006a8ff1cb508d808b78
Daniel Olson <dolson@silver1.mcs.anl.gov>
2012-08-21 17:02:21 -0500

    fixed mongo install

--- kb_mongodb/install-mongodb.sh
+++ kb_mongodb/install-mongodb.sh
@@ -2,7 +2,7 @@
 curl -O "http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-2.0.7.tgz"
 tar -xzf mongodb-linux-x86_64-2.0.7.tgz
 pushd `pwd`
-cd mongodb-linux-x86_64-2.0.7.tgz
+cd mongodb-linux-x86_64-2.0.7
 cp -r bin /kb/runtime/bin
 popd
 
