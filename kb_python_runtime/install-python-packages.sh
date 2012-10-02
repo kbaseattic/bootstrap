47590017fe0ebab0831f8d43b8028ad359347d87
Shane Canon <scanon@lbl.gov>
2012-10-02 10:52:31 -0500

    Added dependencies (requests and rsa) Move django-piston to easy_install.  Doing a specific version of django-piston

--- kb_python_runtime/install-python-packages.sh
+++ kb_python_runtime/install-python-packages.sh
@@ -4,3 +4,8 @@ for P in `cat ./python-pip-list`; do
 	echo "pip installing $P"
 	pip install $P --upgrade
 done
+
+for P in `cat ./python-easy-list`; do
+	echo "easy_installing $P"
+	easy_install $P
+done
