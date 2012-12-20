c8b0d994ec6b89c22842b1a5adc172dd78c81e37
Robert Olson <olson@mcs.anl.gov>
2012-12-20 11:20:06 -0600

    More target-setting support. Use curl instead of wget in install-qiime since mac doesn't have wget by default. Add python-building support to kb_python_runtime.

--- kb_qiime/install-qiime.sh
+++ kb_qiime/install-qiime.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
-if [[ $# != 1 ]] ; then
-    echo "Usage: $0 python-qiime-list" 1>&2
+python=python
+target=/kb/runtime
+
+if [[ $# -eq 1 ]] ; then
+    pkg_list=$1
+elif [[ $# -eq 2 ]] ; then
+    pkg_list=$1
+    target=$2
+    python=$target/bin/python
+else 
+    echo "Usage: $0 python-qiime-list [target-runtime-dir]" 1>&2
     exit 1
 fi
 
-pkg_list=$1
 IFS=$'\t'
 
 while read PKG TGZ URL; do
     echo "###### downloading $PKG ######"
-    wget $URL
+    if [[ ! -f $TGZ ]] ; then
+	    curl -L -O $URL
+    fi
     tar zxf $TGZ
-    pushd $PKG
+    if [[ ! -d $PKG ]] ; then
+	echo "Download of $URL and extraction of $TGZ did not create $PKG directory" 1>&2
+	exit 2
+    fi
     echo "###### installing $PKG ######"
-    python setup.py install
-    popd
-    rm $TGZ
-    rm -rf $PKG
+    (cd $PKG; $python setup.py install)
 done < $pkg_list
 
-pushd bin
-for B in `ls`; do 
-    cp $B /kb/runtime/bin/.
+for B in bin/*; do 
+    echo "install $B"
+    cp $B $target/bin/.
 done
-popd
