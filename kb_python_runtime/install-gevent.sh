caaa883efbb08f5247f660ea414ac97284084e57
Gavin Price <gaprice@lbl.gov>
2013-06-23 13:44:13 -0700

    Add gevent tarball directly to bootstrap, d/l gone

--- kb_python_runtime/install-gevent.sh
+++ kb_python_runtime/install-gevent.sh
@@ -4,7 +4,7 @@ if [ $# -eq 0 ] ; then
 	exit 1
 fi
 target=$1
-curl http://cloud.github.com/downloads/SiteSupport/gevent/gevent-1.0rc2.tar.gz > gevent-1.0rc2.tar.gz
+#curl http://cloud.github.com/downloads/SiteSupport/gevent/gevent-1.0rc2.tar.gz > gevent-1.0rc2.tar.gz
 tar -xvf gevent-1.0rc2.tar.gz 
 cd gevent-1.0rc2
 python setup.py install
