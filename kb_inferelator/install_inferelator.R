057fdcddcd4e20f6a2bb4cb8e4b4c83beae36bb6
Alexey Kazakov <aekazakov@lbl.gov>
2014-01-08 11:24:18 -0800

    changed Inferelator installation script

--- kb_inferelator/install_inferelator.R
+++ kb_inferelator/install_inferelator.R
@@ -1,5 +1,1 @@
-#!Rscript
-chooseCRANmirror(74);
-install.packages('devtools', repos = "http://cran.cnr.berkeley.edu", dep=T);
-library('devtools');
-install_github('cMonkeyNwInf','dreiss-isb',subdir='cMonkeyNwInf');
+devtools::install_github('cMonkeyNwInf','dreiss-isb',subdir='cMonkeyNwInf');
