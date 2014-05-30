75ee8a8d62fd34720697df29fae645757cbf5208
Travis Harrison <guumby@gmail.com>
2014-05-30 13:51:25 -0500

    install script for cd-hit

--- kb_bowtie/install-bowtie2.sh
+++ kb_bowtie/install-bowtie2.sh
@@ -5,13 +5,14 @@ echo "using $dest as installation directory";
 mkdir -p $dest
 
 # downlownload version
+VERSION='2.2.2'
+rm -rf bowtie2-${VERSION}*
+wget "http://sourceforge.net/projects/bowtie-bio/files/bowtie2/${VERSION}/bowtie2-${VERSION}-source.zip"
+unzip bowtie2-${VERSION}-source.zip
 
-rm -rf bowtie2-2.2.2*
-wget "http://sourceforge.net/projects/bowtie-bio/files/bowtie2/2.2.2/bowtie2-2.2.2-source.zip"
-unzip bowtie2-2.2.2-source.zip
-pushd bowtie2-2.2.2
+# compile and copy binaries
+pushd bowtie2-${VERSION}
 make
 cp bowtie2 bowtie2-align-l bowtie2-align-s bowtie2-build bowtie2-build-l bowtie2-build-s bowtie2-inspect bowtie2-inspect-l bowtie2-inspect-s $dest/bin
 popd
-rsync -avz --exclude '*.h' --exclude '*.cpp' bowtie2-2.2.2 $dest
 
---
+++ kb_cdhit/install-cd-hit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+dest=${TARGET-/kb/runtime}
+echo "using $dest as installation directory";
+mkdir -p $dest
+
+# downlownload version
+VERSION='v4.6.1-2012-08-27'
+rm -rf cd-hit-${VERSION}*
+wget "https://cdhit.googlecode.com/files/cd-hit-${VERSION}.tgz"
+unzip cd-hit-${VERSION}-source.zip
+
+# compile and copy binaries
+pushd cd-hit-${VERSION}
+make
+cp cd-hit cd-hit-est cd-hit-2d cd-hit-est-2d cd-hit-div cd-hit-454 $dest/bin
+popd
+
