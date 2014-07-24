ff55363224036959acb743ca1e962f74a070e085
Daniel Olson <dolson@mcs.anl.gov>
2014-07-24 09:44:38 -0700

    added emmax

---
+++ kb_emmax/install-emmax.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+dest=${TARGET-/kb/runtime}
+
+if [ $# -gt 0 ] ; then
+    dest=$1
+    shift
+fi
+
+if [ ! -d "$dest/bin" ] ; then
+    mkdir $dest/bin 
+    echo "$dest/bin created" 1>&2
+fi
+
+if [ -d "/Library" ] ; then
+    echo "NOT supported on OSX"
+    exit 0
+else
+    url="http://www.sph.umich.edu/csg/kang/emmax/download/emmax-intel-binary-20120210.tar.gz"
+fi
+
+file=`basename $url`
+dir=`echo $file | sed -e 's/\.tgz$//'`
+
+curl -f -o $file $url
+if [ $? -ne 0 -o ! -f $file ] ; then
+    echo "Curl of $url to $file failed" 1>&2
+    exit 1
+fi
+
+tar -xzf $file
+
+if [ $? -ne 0 -o ! -f "$dir/emmax-intel64" ] ; then
+    echo "Unpack of $file to $dir failed" 1>&2
+    exit 1
+fi
+
+cp $dir/emmax-intel64 $dest/bin/emmax
+cp $dir/emmax-kin-intel64 $dest/bin/emmax-kin
