a4a6fbb7db6c6c4cacff4aba0487ba44932b5718
Keith Keller <kkeller@lbl.gov>
2015-02-10 14:28:16 -0800

    Added fastQValidator

---
+++ kb_fastqvalidator/fastqvalidator_build.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+. ../tools/error_handler
+
+trap 'error ${LINENO}' ERR
+
+target=${TARGET-/kb/runtime}
+
+if [[ $# -ne 0 ]] ; then
+        target=$1
+        shift
+fi
+
+IDIR=$target
+libStatGenURL='https://github.com/statgen/libStatGen'
+fastQValidatorURL='https://github.com/statgen/fastQValidator'
+# the official releases are too old, use a github tag to
+# avoid tracking master
+libStatGenTag=''
+fastQValidatorTag=''
+
+
+echo "###### cloning $libStatGenURL ######"
+git clone $libStatGenURL
+echo "###### cloning $fastQValidatorURL ######"
+git clone $fastQValidatorURL
+
+echo "###### building $libStatGenURL ######"
+rm -rf libStatGen
+cd libStatGen
+git checkout $libStatGenTag
+make
+cd ..
+
+echo "###### building $fastQValidatorURL ######"
+rm -rf fastQValidator
+cd fastQValidator
+git checkout $fastQValidatorTag
+make
+cd ..
+
+
+echo "###### installing $fastQValidatorURL ######"
+cp ./fastQValidator/bin/fastQValidator $IDIR/bin
+
+rm -rf libStatGen fastQValidator
