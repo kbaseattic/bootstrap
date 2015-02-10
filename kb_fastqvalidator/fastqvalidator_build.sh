afad28d11b668d8da6edd8ee72d6d3c205a4552d
Keith Keller <kkeller@lbl.gov>
2015-02-10 14:37:49 -0800

    Added tags (10 feb 2015), explicit binary name

--- kb_fastqvalidator/fastqvalidator_build.sh
+++ kb_fastqvalidator/fastqvalidator_build.sh
@@ -16,8 +16,8 @@ libStatGenURL='https://github.com/statgen/libStatGen'
 fastQValidatorURL='https://github.com/statgen/fastQValidator'
 # the official releases are too old, use a github tag to
 # avoid tracking master
-libStatGenTag=''
-fastQValidatorTag=''
+libStatGenTag='05f2a7e8bb8d0aa47d8814076c69df0b7cf44aeb'
+fastQValidatorTag='2e8bfa61fb2cfb5f3ff4568ff0590228a7c6c1e7'
 
 rm -rf libStatGen fastQValidator
 
@@ -40,6 +40,6 @@ cd ..
 
 
 echo "###### installing $fastQValidatorURL ######"
-cp ./fastQValidator/bin/fastQValidator $IDIR/bin
+cp ./fastQValidator/bin/fastQValidator $IDIR/bin/fastQValidator
 
 rm -rf libStatGen fastQValidator
